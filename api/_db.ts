9e00830df0d2d9d7dd423697b4e2784dcb86a8c6
Nicholas Tan <nicorasu.tan@gmail.com>
2026-03-03 08:38:46 +0800

    fix(api): defer database url check to prevent vercel 500 module crash

--- api/_db.ts
+++ api/_db.ts
@@ -2,11 +2,11 @@ import { neon } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
 import * as schema from '../src/db/schema.js';
 
-const databaseUrl = process.env.DATABASE_URL;
+const databaseUrl = process.env.DATABASE_URL || '';
+
+// Delay throwing to allow the module to load on Vercel Edge so we can debug it
+export const db = databaseUrl ? drizzle(neon(databaseUrl), { schema }) : {} as any;
 
 if (!databaseUrl) {
-    throw new Error('DATABASE_URL is not defined in API');
+    console.error('DATABASE_URL is not defined in API Environment!!');
 }
-
-const sql = neon(databaseUrl);
-export const db = drizzle(sql, { schema });
